1c5dfd4f08d5b1b87d9b23b1faa10164775fa63c
Hasindu Lakshan <hasindulakshan0040@gmail.com.com>
2024-08-28 11:36:47 +0530

    Environment Setup

---
+++ database/database.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let database;
+const mongoURI = process.env.MONGODB_URI;
+
+
+async function connectTo(){
+    const connection =  await MongoClient.connect(mongoURI);
+    database = connection.db('medimate');
+}
+
+
+function DbConn(){
+    if(!database){
+        throw new Error('not connected');
+    }
+    return database;
+}
+
+module.exports = {
+    connectTo : connectTo,
+    DbConn: DbConn
+}
