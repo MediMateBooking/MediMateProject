74038643f50b68211f5df77d9b9f556656092c16
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-09-19 21:12:54 +0530

    add try-catch to mailer.js

--- mail/mailer.js
+++ mail/mailer.js
@@ -1,30 +1,40 @@
 const nodemailer = require("nodemailer");
+const path = require('path');
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    
-    user: "zoomclasss63@gmail.com",
-    pass: "ytqytxzezodjsvow",
+    user: process.env.USER,
+    pass: process.env.APP_PASSWORD,
   },
 });
 
-async function mainEmail(address,userIdURL) {
-  
+async function mainEmail(address, userIdURL) {
 
   const receiver = address || "chamoddousl@gmail.com"
 
-  const info = await transporter.sendMail({
-    from: 'MediMate E-Channeling Service', // sender address
-    to: receiver, // list of receivers
-    subject: "Welcome to MediMate", // Subject line
-    text: userIdURL, // plain text body
-    html: "<b>"+userIdURL+"</b>", // html body
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: 'MediMate E-Channeling Service', // sender address
+      to: receiver, // list of receivers
+      subject: "Welcome to the MediMate Appointment", // Subject line
+      text: userIdURL, // plain text body
+      html: "<b>" + userIdURL + "</b>", // html body
+      attachments: [
+        {
+          filename: 'MediMate.png',
+          path: path.join(__dirname, 'favicon.png'),
+          contentType: 'image/png'
+        }
+      ]
+    });
 
-  console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.log("Error sending email:", error);
+  }
 
 }
 
