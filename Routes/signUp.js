f89b93a7af9575aa75fc6f483d1b706ccc09460d
chamod Basnayake <chamoddousl@gmail.com>
2024-10-01 08:23:26 +0530

    profile activation

--- Routes/signUp.js
+++ Routes/signUp.js
@@ -25,28 +25,18 @@ router.get('/signup', (req, res) => {
 
 router.post('/signup/doctors', async (req, res) => {
 
-    const { userName, email, password ,confirmPassword, slmcregi,role} = req.body;
-    console.log(userName, email, password,confirmPassword,slmcregi,role);
+    const { userName, email, slmcregi,role} = req.body;
+    console.log(userName, email,slmcregi,role);
 
-    if(userName === '' || email === '' || password === '' || confirmPassword === '' || slmcregi === ''){
+    if(userName === '' || email === '' || slmcregi === ''){
         return res.json({ success: false, message: 'Please Fill All Fields' });
     }
     
-    if(password !== confirmPassword){
-        return res.json({ success: false, message: 'Confirm Password Not Matched' });
-    }
-
-    if(password.length < 5){
-        return res.json({ success: false, message: 'Password Should Greater than 5 charactors' });
-    }
-
     const existingUserEmail =  await db.DbConn().collection('doctors').findOne({email:email});
     if(existingUserEmail){
 
         return res.json({ success: false, message: email+' is already used' });
     }
-
-    const hashedpass = await bcryptjs.hash(password,12);
 
     let userIdURL = uuid.v4();
 
@@ -54,37 +44,28 @@ router.post('/signup/doctors', async (req, res) => {
         name: userName,
         email: email,
         userID : userIdURL,
-        password: hashedpass,
         slmcregi:slmcregi,
         role:role,
-        profileActive : false
+        profileActive : false,
+        prifileApprove : false
     };
 
     try {
 
         const userResult = await db.DbConn().collection('doctors').insertOne(newUser);
+        console.log('Successfully Saved');
+        res.json({ success: true, message: 'Your Account sent for Approving. We will let you know process is completed.'  })
         
     } catch (error) {
         console.error("Server Error:", error);
         res.json({ success: false, message: 'Server Error' });
     }
 
-
-    let userURL = 'hello, this is activation message'
-
-    try {
-        await mailer.mainEmail(email, userURL); 
-        console.log("Activation Email sent to "+email);
-        res.json({ success: true, message: 'Activation Email sent to '+email  })
-      } catch (error) {
-        console.error("Error sending email:", error);
-        res.json({ success: false, message: 'Check Your Email Address' });
-      }
-
 });
 
 
 router.post('/signup/patients', async (req, res) => {
+
 
     const { userName, email, password ,confirmPassword, role} = req.body;
     console.log(userName, email, password,confirmPassword,role);
@@ -110,6 +91,7 @@ router.post('/signup/patients', async (req, res) => {
     const hashedpass = await bcryptjs.hash(password,12);
 
     let userIdURL = uuid.v1();
+    const expires = Date.now() + 3600000;
 
     const newUser = {
         name: userName,
@@ -117,7 +99,8 @@ router.post('/signup/patients', async (req, res) => {
         userID : userIdURL,
         password: hashedpass,
         role:role,
-        profileActive : false
+        profileActive : false,
+        linkExpire : expires
     };
 
     try {
@@ -129,7 +112,7 @@ router.post('/signup/patients', async (req, res) => {
         res.json({ success: false, message: 'Server Error' });
     }
 
-    let userURL = 'hello, this is activation message'
+    let userURL = `http://localhost:4000/user/${userIdURL}`
 
     try {
         await mailer.mainEmail(email, userURL); 
