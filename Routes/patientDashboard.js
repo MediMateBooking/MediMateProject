535f5b3c02de2fbaffd40dd827e02dd7d2afe42b
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-10-09 21:47:44 +0530

    data binding

--- Routes/patientDashboard.js
+++ Routes/patientDashboard.js
@@ -4,24 +4,22 @@ const crypto = require("crypto");
 
 const db = require("../database/database");
 
-router.get("/patient/:id", async(req, res) => {
-  
+router.get("/patient/:id", async (req, res) => {
   try {
     const userID = req.params.id;
 
-    const currentPatient = await db.DbConn().collection("patients").find({ userID: userID }).toArray();
-  
-    if(currentPatient.length === 0) throw new Error ('cannot find User')
+    const currentPatient = await db
+      .DbConn()
+      .collection("patients")
+      .find({ userID: userID })
+      .toArray();
 
-     res.render('patientDashboard',{currentPatient : currentPatient});
+    if (currentPatient.length === 0) throw new Error("cannot find User");
 
-
+    res.render("patientDashboard", { currentPatient: currentPatient });
   } catch (error) {
     res.status(500).send(`<h1>Server Error</h1><p>${error.message}</p>`);
   }
-
-
-
 });
 
 module.exports = router;
--- public/scripts/resizePosition2.js
+++ public/scripts/resizePosition2.js
@@ -3,23 +3,17 @@ if (document.querySelector(".statusMessage p")) {
   const viewportWidth = window.innerWidth;
   const elementWidth = element.offsetWidth;
 
-    const leftPosition = (viewportWidth - elementWidth) / 2;
-    element.style.left = `${leftPosition}px`;
-    element.style.visibility = 'visible';
+  const leftPosition = (viewportWidth - elementWidth) / 2;
+  element.style.left = `${leftPosition}px`;
+  element.style.visibility = "visible";
+}
 
-    }
+if (document.querySelector(".validationSucess p")) {
+  const element = document.querySelector(".validationSucess p");
+  const viewportWidth = window.innerWidth;
+  const elementWidth = element.offsetWidth;
 
-
-    if(document.querySelector('.validationSucess p')){
-
-        const element = document.querySelector('.validationSucess p');
-        const viewportWidth = window.innerWidth;
-        const elementWidth = element.offsetWidth;
-    
-        const leftPosition = (viewportWidth - elementWidth) / 2;
-        element.style.left = `${leftPosition}px`;
-        element.style.visibility = 'visible';
-    
-        }
-
-
+  const leftPosition = (viewportWidth - elementWidth) / 2;
+  element.style.left = `${leftPosition}px`;
+  element.style.visibility = "visible";
+}
