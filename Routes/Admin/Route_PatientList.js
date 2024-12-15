edb52a695b17449a15687b632e22afbca5a71cf9
Achintha Kodithuwakku <achintha17423@gmail.com>
2024-12-15 14:23:24 +0530

    The UI of the Appointment and PatientList subpages of the admin dashboard is finished

---
+++ Routes/Admin/Route_PatientList.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../../database/database");
+router.get("/admin/patientList/:id", async(req, res) => {
+  try {
+
+    const dynamicRouteCode = req.params.id;
+    
+        const activeAdmin = await db
+          .DbConn()
+          .collection("admin")
+          .find({ dynamicRouteCode: dynamicRouteCode })
+          .toArray();
+
+    if (activeAdmin.length === 0) throw new Error("cannot find Admin");
+
+    res.render("Admin/patientList",{activeAdmin : activeAdmin});
+  } catch (error) {
+    res.status(500).send(`<h1>Server Error</h1><p>${error.message}</p>`);
+  }
+});
+
+
+module.exports = router;
