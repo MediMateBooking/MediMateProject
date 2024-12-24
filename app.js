272ce46380f9d4f236731018b1035b8f52d6b9a3
chamod Basnayake <chamoddousl@gmail.com>
2024-12-24 21:13:29 +0530

    patient profile

--- app.js
+++ app.js
@@ -42,8 +42,11 @@ const db = require("./database/database");
 
 app.use(express.static("public"));
 app.use("/patient/images", express.static("images"));
+app.use("/patient/profile/images", express.static("images"));
+app.use("/patient/changePassword/images", express.static("images"));
 app.use("/doctor/images", express.static("images"));
 app.use("/doctor/profile/images", express.static("images"));
+app.use("/doctor/password/images", express.static("images"));
 app.use("/admin/doctors/images", express.static("images"));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
