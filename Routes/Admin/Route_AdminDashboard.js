a5375ab138e28e1ed993e0646901f0f1ec29b1f7
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2025-03-14 06:11:44 +0530

    delete operation

--- Routes/Admin/Route_AdminDashboard.js
+++ Routes/Admin/Route_AdminDashboard.js
@@ -13,9 +13,44 @@ router.get("/admin/:id", async (req, res) => {
       .find({ dynamicRouteCode: dynamicRouteCode })
       .toArray();
 
+    const totalPatient = await db
+      .DbConn()
+      .collection("patients")
+      .find()
+      .toArray();
+  
+    const totalDoctors = await db
+      .DbConn()
+      .collection("doctors")
+      .find()
+      .toArray();
+
+    const totalAppointments = await db
+      .DbConn()
+      .collection("appointments")
+      .find()
+      .toArray();
+
+  const totalReviews= await db
+      .DbConn()
+      .collection("reviews")
+      .find()
+      .toArray();
+
     if (activeAdmin.length === 0) throw new Error("cannot find Admin");
 
-    res.render("Admin/adminDashboard", { activeAdmin: activeAdmin });
+    res.render("Admin/adminDashboard", 
+      { activeAdmin: activeAdmin,
+        totalPatient: totalPatient,
+        totalPatientCount : totalPatient.length,
+        totalDoctors:totalDoctors,
+        totalDoctorsCount : totalDoctors.length,
+        totalAppointments:totalAppointments,
+        totalAppointmentsCount : totalAppointments.length,
+        totalReviewsCount : totalReviews.length
+
+
+      });
   } catch (error) {
     res.render("common/500");
   }
